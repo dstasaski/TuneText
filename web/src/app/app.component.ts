751ab181b8cd8300066ed19dba08454320d76f98
dennystasaski <dstasaski@gmail.com>
2019-04-20 10:51:34 -0500

    adding inital homepage structure

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'web';
+  title = 'MoodyBeats';
+  clickDefault = 'Weather information:'
+  clickMessage = ''
+
+  onClickMe() {
+    this.clickMessage = this.clickDefault + 'button clicked!';
+  }
 }
