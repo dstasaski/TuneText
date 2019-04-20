e481fcab4d38f9618ea48247735f40dfc89910b6
dennystasaski <dstasaski@gmail.com>
2019-04-20 17:02:02 -0500

    docker added for api and docker-compose working

--- api/app.py
+++ api/app.py
@@ -5,9 +5,14 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
+
 @app.route('/api/weather')
 def hello_world():
     data = {}
     data['description'] = 'Sunny, 100F (mock)'
-    return jsonify(data)
+    response = jsonify(data)
+    return response
 
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
--- web/src/app/api.service.ts
+++ web/src/app/api.service.ts
@@ -9,7 +9,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class ApiService {
-  configUrl = 'http://127.0.0.1:5000/api/weather'
+  configUrl = 'http://localhost:5000/api/weather'
   constructor(private http: HttpClient) { }
 
   getWeather():Observable<WeatherResponse> {
