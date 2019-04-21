235022104c21ef5c2646d22d0d5dac45f871ea29
dennystasaski <dstasaski@gmail.com>
2019-04-21 16:05:48 -0500

    getting token programatically from spotify

--- api/spotify.py
+++ api/spotify.py
@@ -1,14 +1,23 @@
-# import requests
+import requests
 
 
 class Spotify:
+    token_url = 'https://accounts.spotify.com/api/token'
+
     def __init__(self):
         self.CLIENT_ID, self.CLIENT_SECRET = self.read_secrets()
+        self.token_expire = 0
 
-    def read_secrets(self):
+    @staticmethod
+    def read_secrets():
         with open('secret.txt') as f:
             return f.readline().strip().split(':')
 
+    def get_token(self):
+        # TODO add token expire logic
+        data = {'grant_type': 'client_credentials'}
+        response = requests.post(self.token_url, auth=(self.CLIENT_ID, self.CLIENT_SECRET), data=data)
+        return response.json()['access_token']
+
     def browse_category(self, category):
-        # print(self.CLIENT_ID, self.CLIENT_SECRET, category)
-        return {'CLIENT_ID': self.CLIENT_ID, 'CLIENT_SECRET': 'ahha'}
+        return {'token': self.get_token()}
