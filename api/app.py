69b79766d3b94245381904114eeb61d7a6d136a2
dennystasaski <dstasaski@gmail.com>
2019-04-29 19:54:37 -0500

    add google cloud mp3 api call.  redisn webpage

--- api/app.py
+++ api/app.py
@@ -2,9 +2,13 @@ from flask import Flask
 from flask import jsonify
 from flask_cors import CORS
 from spotify import Spotify
+from googlecloud import GoogleAPI
 
 app = Flask(__name__)
 CORS(app)
+
+spotify = Spotify()
+googleAPI = GoogleAPI()
 
 
 @app.route('/api/weather')
@@ -17,8 +21,12 @@ def weather():
 
 @app.route('/api/music/browse/categories/<category>')
 def music_category(category):
-    spot = Spotify()
-    return jsonify(spot.browse_category(category))
+    return jsonify(spotify.browse_category(category))
+
+
+@app.route('/api/text_to_speech/<text>')
+def speech_mp3(text):
+    return jsonify(googleAPI.text_to_speech(text))
 
 
 if __name__ == '__main__':
---
+++ api/googlecloud.py
@@ -0,0 +1,30 @@
+import requests
+
+
+class GoogleAPI:
+    speech_url = 'https://texttospeech.googleapis.com/v1/text:synthesize?fields=audioContent&key={token}'
+
+    def __init__(self):
+        self.API_TOKEN = self.cloud_token()
+
+    @staticmethod
+    def cloud_token():
+        with open('cloud.txt') as f:
+            return f.readline().strip()
+
+    def text_to_speech(self, text):
+        body = {
+             "input": {
+                "text": text
+             },
+             "voice": {
+                "languageCode": "en-US",
+                "ssmlGender": "female"
+             },
+             "audioConfig": {
+                "audioEncoding": "mp3"
+             }
+        }
+        url = self.speech_url.replace('{token}', self.API_TOKEN)
+        response = requests.post(url=url, json=body)
+        return response.json()
