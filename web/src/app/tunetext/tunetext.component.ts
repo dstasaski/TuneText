30e5c4377d1505b21d1d3c18cddcf989e77b92d9
dennystasaski <dstasaski@gmail.com>
2019-04-29 20:38:35 -0500

    add text speech in website

--- web/src/app/tunetext/tunetext.component.ts
+++ web/src/app/tunetext/tunetext.component.ts
@@ -1,19 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-tunetext',
   templateUrl: './tunetext.component.html',
-  styleUrls: ['./tunetext.component.css']
+  styleUrls: ['./tunetext.component.css'],
+  providers: [ApiService]
 })
 export class TunetextComponent implements OnInit {
-  userText = '';
-
-  constructor() { }
+  constructor(private apiService: ApiService) { }
 
   ngOnInit() {
   }
 
   onSubmit(input:string) {
-    this.userText = input;
+    this.apiService.getTextMP3(input).subscribe(data => {
+      this.playAudio(data.audioContent);
+    });
+  }
+
+  playAudio(encodedAudio:string) {
+    var audio = new Audio("data:audio/mp3;base64," + encodedAudio);
+    audio.play();
   }
 }
