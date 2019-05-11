1b86b9333d1f861f802693c6925e5e99ef80d60d
dennystasaski <dstasaski@gmail.com>
2019-05-11 17:01:48 -0500

    add basic subjectivity trainer

--- trainer/trainer.py
+++ trainer/trainer.py
@@ -2,7 +2,41 @@ from nltk.classify import NaiveBayesClassifier
 from nltk.corpus import subjectivity
 from nltk.sentiment import SentimentAnalyzer
 from nltk.sentiment.util import *
+
 import pickle
+import re
+
+nltk.download('subjectivity')
+
+
+n_instances = 100
+subj_docs = [(sent, 'subj') for sent in subjectivity.sents(categories='subj')[:n_instances]]
+obj_docs = [(sent, 'obj') for sent in subjectivity.sents(categories='obj')[:n_instances]]
+
+train_subj_docs = subj_docs[:80]
+test_subj_docs = subj_docs[80:100]
+train_obj_docs = obj_docs[:80]
+test_obj_docs = obj_docs[80:100]
+training_docs = train_subj_docs+train_obj_docs
+testing_docs = test_subj_docs+test_obj_docs
+
+sentim_analyzer = SentimentAnalyzer()
+all_words_neg = sentim_analyzer.all_words([mark_negation(doc) for doc in training_docs])
+unigram_feats = sentim_analyzer.unigram_word_feats(all_words_neg, min_freq=4)
+sentim_analyzer.add_feat_extractor(extract_unigram_feats, unigrams=unigram_feats)
+
+training_set = sentim_analyzer.apply_features(training_docs)
+test_set = sentim_analyzer.apply_features(testing_docs)
+trainer = NaiveBayesClassifier.train
+
+classifier = sentim_analyzer.train(trainer, training_set)
+for key,value in sorted(sentim_analyzer.evaluate(test_set).items()):
+    print('{0}: {1}'.format(key, value))
+
+
+
+
+'''
 
 # tokenize, remove special char, remove stopwords
 # classify emotion
@@ -10,13 +44,6 @@ import pickle
 # apply supervised algorithm
 # calculate accuracy
 
-
-
-
-
-
-
-'''
 save_classifier = open("naivebayes.pickle","wb")
 pickle.dump(classifier, save_classifier)
 save_classifier.close()
@@ -25,4 +52,5 @@ save_classifier.close()
 classifier_f = open("naivebayes.pickle", "rb")
 classifier = pickle.load(classifier_f)
 classifier_f.close()
+
 '''
