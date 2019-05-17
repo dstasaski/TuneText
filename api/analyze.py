69b594a7e099ca9d137152c684acbb26e8d24cd6
dennystasaski <dstasaski@gmail.com>
2019-05-16 20:45:12 -0500

    analyzing text correctly

--- api/analyze.py
+++ api/analyze.py
@@ -1,12 +1,20 @@
 from flask import send_file
-import pickle
-import nltk
+from nltk.stem import PorterStemmer
+from nltk.corpus import stopwords
+from nltk.sentiment.util import *
+
 
 class Analyzer:
     def __init__(self):
+        nltk.download('stopwords')
+        nltk.download('punkt')
+        
         classifier_f = open("models/naivebayes.pickle", "rb")
         self.classifer = pickle.load(classifier_f)
         classifier_f.close()
+
+        self.stemmer = PorterStemmer()
+        self.stop_words = set(stopwords.words('english'))
 
     def song(self):
         return send_file('assets/IceCream.mp3', mimetype='audio/mpeg')
@@ -20,5 +28,6 @@ class Analyzer:
         return response
 
     def find_sentiment(self, text):
-        sentiment = self.classifer.classify(text)
+        features = [self.stemmer.stem(word) for word in mark_negation(nltk.word_tokenize(text)) if word not in self.stop_words]
+        sentiment = self.classifer.classify(features)
         return sentiment
