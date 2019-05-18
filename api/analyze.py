6d0fa51a6a1a305ed03f9bef694a79f557d8c830
dennystasaski <dstasaski@gmail.com>
2019-05-18 09:53:06 -0500

    restructure assets

--- api/analyze.py
+++ api/analyze.py
@@ -6,9 +6,6 @@ from nltk.sentiment.util import *
 
 class Analyzer:
     def __init__(self):
-        nltk.download('stopwords')
-        nltk.download('punkt')
-
         classifier_f = open("models/naivebayes.pickle", "rb")
         self.classifer = pickle.load(classifier_f)
         classifier_f.close()
@@ -23,7 +20,7 @@ class Analyzer:
             return f.readlines()
 
     def song(self):
-        return send_file('assets/IceCream.mp3', mimetype='audio/mpeg')
+        return send_file('assets/songs/IceCream.mp3', mimetype='audio/mpeg')
 
     def smart_song(self, text):
         sentiment = self.find_sentiment(text)
@@ -39,7 +36,7 @@ class Analyzer:
         return 'fdsv234gn35'
 
     def find_sentiment(self, text):
-        features = \
-            [self.stemmer.stem(word) for word in mark_negation(nltk.word_tokenize(text)) if word not in self.stop_words]
+        tokens = mark_negation(nltk.word_tokenize(text.lower()))
+        features = [self.stemmer.stem(word) for word in tokens if word not in self.stop_words]
         sentiment = self.classifer.classify(features)
         return sentiment
