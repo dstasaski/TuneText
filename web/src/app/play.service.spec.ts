8e75244f29bab1c87e6a2e7ce0f000f6936f92ec
dennystasaski <dstasaski@gmail.com>
2019-06-16 17:27:08 -0500

    fix other angular test

--- web/src/app/play.service.spec.ts
+++ web/src/app/play.service.spec.ts
@@ -2,14 +2,13 @@ import { TestBed } from '@angular/core/testing';
 import { HttpClientModule } from '@angular/common/http';
 import { PlayService } from './play.service';
 import { RouterTestingModule } from '@angular/router/testing';
-import { Routes, RouterModule } from '@angular/router';
+
 
 describe('PlayService', () => {
   beforeEach(() => TestBed.configureTestingModule({
     imports: [
       HttpClientModule,
-      RouterTestingModule,
-      RouterModule.forRoot([])
+      RouterTestingModule
     ]
   }));
 
--- web/src/app/playresolver.service.spec.ts
+++ web/src/app/playresolver.service.spec.ts
@@ -1,10 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 import { HttpClientModule } from '@angular/common/http';
 import { PlayresolverService } from './playresolver.service';
+import { RouterTestingModule } from '@angular/router/testing';
+
 
 describe('PlayresolverService', () => {
   beforeEach(() => TestBed.configureTestingModule({
-    imports: [HttpClientModule]
+    imports: [
+      HttpClientModule,
+      RouterTestingModule
+    ]
   }));
 
   it('should be created', () => {
