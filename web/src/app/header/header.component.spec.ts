9f5a8bdb7a5f940228d62d18d7957f512fe1db85
dennystasaski <dstasaski@gmail.com>
2019-07-19 16:28:15 -0500

    fix unit tests

--- web/src/app/header/header.component.spec.ts
+++ web/src/app/header/header.component.spec.ts
@@ -1,6 +1,9 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpClientModule } from '@angular/common/http';
 import { HeaderComponent } from './header.component';
+import { FormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
+
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
@@ -9,7 +12,11 @@ describe('HeaderComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ HeaderComponent ],
-      imports: [HttpClientModule],
+      imports: [
+        HttpClientModule,
+        FormsModule,
+        RouterTestingModule
+      ],
       providers: []
     })
     .compileComponents();
